fe36eb0ae9e6947333bc544d8cf52828d1d28e49
garrettdick <garrettd0511@gmail.com>
2022-05-02 10:45:07 -0500

    RadioEye changes

--- RadioEye.js
+++ RadioEye.js
@@ -4,29 +4,29 @@
 // 4) D 
 
 
-5) function listener(){
-  let div = document.createElement('div')
-  div.addEventListener('keypress', () => {
-        alert('I have been selected using the keyboard')
-    })
-}
+// 5) function listener(){
+//   let div = document.createElement('div')
+//   div.addEventListener('keypress', () => {
+//         alert('I have been selected using the keyboard')
+//     })
+// }
 
-6) function setRole(){
-    let headingElements = document.querySelectorAll('h1','h2','h3','h4')
-    headingElements.role = 'presentation'
-}
+// 6) function setRole(){
+//     let headingElements = document.querySelectorAll('h1','h2','h3','h4')
+//     headingElements.role = 'presentation'
+// }
 
-7) function startCountdown(a,b){
-    let sum = a + b 
-    const interval = setInterval(() => {
-        console.log(sum)
-        sum--
+// 7) function startCountdown(a,b){
+//     let sum = a + b 
+//     const interval = setInterval(() => {
+//         console.log(sum)
+//         sum--
 
-        if (sum < 0){
-            clearInterval(interval)
-            console.log('Done!')
-        }
-    }, 1000)
-}
+//         if (sum < 0){
+//             clearInterval(interval)
+//             console.log('Done!')
+//         }
+//     }, 1000)
+// }
 
-console.log(startCountdown(1,4))
+// console.log(startCountdown(1,4))
