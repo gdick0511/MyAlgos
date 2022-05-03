cfe5fa5abe61e5ec13c0afc8f080df5cf5972362
garrettdick <garrettd0511@gmail.com>
2022-05-03 16:45:09 -0500

    added interview questions

--- RadioEye.js
+++ RadioEye.js
@@ -30,3 +30,49 @@
 // }
 
 // console.log(startCountdown(1,4))
+
+// Question 3
+
+// function solution(S) {
+//     // write your code in JavaScript (Node.js 8.9.4)
+//     let maxcount = 0;
+//     let count = 1;
+//     for(let i = 1; i < S.length; i++){
+//         if(S.charAt(i) != S.charAt(i - 1)){ 
+//             maxcount = Math.max(maxcount, count);
+//             count = 1;
+//         }
+//         else{
+//             count++;
+//         }
+//     }
+
+//     maxcount = Math.max(maxcount, count);    
+//     count = 1;
+//     let res = 0;
+//     for(let i = 1; i < S.length; i++){
+//         if(S.charAt(i) != S.charAt(i - 1)){
+//             res = res + maxcount - count;
+//             count = 1;
+//         }
+//         else{
+//             count++;
+//         }
+//     }
+//     res = res + maxcount - count;
+//     return res;
+// }
+
+// Question 2 
+
+// function solution(A, K) {
+//     var n = A.length;
+//     for (var i = 0; i < n - 1; i++) {
+//         if (A[i] + 1 < A[i + 1])
+//             return false;
+//     }
+//     if (A[0] = 1 && A[n - 1] != K)
+//         return false;
+//     else
+//         return true;
+// }
